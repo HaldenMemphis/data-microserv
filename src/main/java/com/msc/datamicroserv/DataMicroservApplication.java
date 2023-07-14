8c94f7611b1f46fc38c45420d63fe0139923349f
HaldenMemphis <jasondennis12138@gmail.com>
2023-07-14 15:27:06 +0100

    -- Story: Create Project -- Author: Yifan Liu

---
+++ src/main/java/com/msc/datamicroserv/DataMicroservApplication.java
@@ -0,0 +1,13 @@
+package com.msc.datamicroserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMicroservApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataMicroservApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msc/datamicroserv/DataMicroservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msc.datamicroserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMicroservApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
